1882e3275360ca8870d27fd3b40b6a1a73ec6378
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-10-15 12:48:15 -0400

    Boilerplate server code

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 const sql = require("mssql");
 
 const config = {
-  password: "",
+  server: process.env.SERVER_NAME,
+  database: process.env.DATABASE_NAME,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
   options: {
+    trustedConnection: true,
+    enableArithAbort: true,
     trustServerCertificate: true,
   },
+};
+
+module.exports = {
+  connect: () => sql.connect(config),
+  sql,
 };
--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const app = express();
 const PORT = 3001;
 
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { connect } = require("./db.js");
 
-let notes = ["1", "2"];
-console.log(process.env.TEST_KEY);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+connect()
+  .then((connection) => {
+    console.log("Connected To Database");
+  })
+  .catch((error) => {
+    console.log("Couldn't Connect To Database");
+    console.log(error);
+  });
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
-});
-
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
 });
 
 app.listen(PORT, () => {
