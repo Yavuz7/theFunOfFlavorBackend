1f8b1e9420bb88a7e1eaa2372991172f9d1e97d5
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-11-05 17:31:52 -0500

    Added validations for body IDs and body Text

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -3,11 +3,25 @@ const { sql } = require("../db.js");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  console.log(req.body);
-  const { StepID, UserID, SuggestionText } = req.body;
-  console.log(SuggestionText);
-  const query = `
+const {
+  bodyIntValidation,
+  bodyTextValidation,
+  validate,
+} = require("../validators.js");
+
+router.post(
+  "/",
+  [
+    ...bodyIntValidation(["StepID"]),
+    ...bodyIntValidation(["UserID"]),
+    ...bodyTextValidation(["SuggestionText"]),
+  ],
+  validate,
+  async (req, res) => {
+    console.log(req.body);
+    const { StepID, UserID, SuggestionText } = req.body;
+    console.log(SuggestionText);
+    const query = `
     INSERT INTO Suggestions (
     StepID, 
     UserID, 
@@ -18,21 +32,22 @@ router.post("/", async (req, res) => {
     @UserID, 
     @SuggestionText
     );`;
-  try {
-    const pool = await sql.connect();
+    try {
+      const pool = await sql.connect();
 
-    const result = await pool
-      .request()
-      .input("StepID", sql.Int, StepID)
-      .input("UserID", sql.Int, UserID)
-      .input("SuggestionText", sql.NVarChar, SuggestionText)
-      .query(query);
+      const result = await pool
+        .request()
+        .input("StepID", sql.Int, StepID)
+        .input("UserID", sql.Int, UserID)
+        .input("SuggestionText", sql.NVarChar, SuggestionText)
+        .query(query);
 
-    res.send(result.recordset);
-    console.log(result.rowsAffected);
-  } catch (error) {
-    console.log(error);
+      res.send(result.recordset);
+      console.log(result.rowsAffected);
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 module.exports = router;
--- validators.js
+++ validators.js
@@ -1,4 +1,4 @@
-const { query, param, validationResult } = require("express-validator");
+const { body, query, param, validationResult } = require("express-validator");
 
 //Validation Setup from: https://dev.to/nedsoft/a-clean-approach-to-using-express-validator-8go
 
@@ -12,23 +12,39 @@ const validate = (req, res, next) => {
 };
 
 //Solution might be silly I'm unsure yet
+//So to have more generic validators I generate arrays,
+//and then need to seperate them when using them for middleware
+//It's kind of messy actually so I may change it
+//But I think it will help a bit for some of the more
+//Tedious or repetitive validations
+//May split up validators to different files for routes if theres too many
 const paramIntValidation = (ID) => {
   return [
-    param(`${ID}`)
-      .isInt()
-      .withMessage(`${ID} is not an integer`)
-      .not()
-      .isEmpty(),
+    param(`${ID}`).notEmpty().isInt().withMessage(`${ID} is not an integer`),
   ];
 };
 
+//For offset variable in search
 const queryIntValidation = (key) => {
   return [
-    query(`${key}`)
-      .isInt()
-      .withMessage(`${key} is not an integer`)
-      .not()
-      .isEmpty(),
+    query(`${key}`).notEmpty().isInt().withMessage(`${key} is not an integer`),
+  ];
+};
+
+//  const { StepID, UserID, SuggestionText } = req.body;
+//Query for suggestions
+const bodyIntValidation = (key) => {
+  return [
+    body(`${key}`).notEmpty().isInt().withMessage(`${key} is not an integer`),
+  ];
+};
+
+const bodyTextValidation = (key) => {
+  return [
+    body(`${key}`)
+      .notEmpty()
+      .isAlphanumeric("en-US", { ignore: " " })
+      .withMessage("Only letters and numbers are allowed."),
   ];
 };
 
@@ -36,4 +52,6 @@ module.exports = {
   validate,
   paramIntValidation,
   queryIntValidation,
+  bodyIntValidation,
+  bodyTextValidation,
 };
